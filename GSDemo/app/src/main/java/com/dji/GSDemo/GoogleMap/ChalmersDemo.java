b6b2dd95ed340b83a7be57e24854c160f65d2963
Globe999 <marten.sanderod@gmail.com>
2023-02-21 12:56:04 +0100

    Added ChalmersDemo

---
+++ GSDemo/app/src/main/java/com/dji/GSDemo/GoogleMap/ChalmersDemo.java
@@ -0,0 +1,13 @@
+package com.dji.GSDemo.GoogleMap;
+
+import android.util.Log;
+import android.view.View;
+
+import androidx.fragment.app.FragmentActivity;
+
+import dji.sdk.flightcontroller.FlightController;
+
+public class ChalmersDemo extends FragmentActivity{
+
+    private FlightController flightController;
+}
